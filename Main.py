1998e9d66efab7530fd667398440090a059eaa05
Jonathan Wilchield Arthur(10945544) <100198275+jocypher@users.noreply.github.com>
2022-08-07 15:27:10 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,11 @@
-# A python program to sum the number of prime numbers from 1 to 100
-for i in range(100):
-  if i % 2 == 1 and i % 3 != 0 :
-    print(i)
+# A python program to find the average of the sum the number of prime numbers from 1 to 10
+prime_numbers = [2,3,5,7]
+sum = prime_numbers[0] + prime_numbers[1] + prime_numbers[2]
+prime = sum + prime_numbers[3] 
+print(prime)
+  
+  # finding the average 
+  
+primes = len(prime_numbers )
+aver = prime/ primes
+print(aver)
