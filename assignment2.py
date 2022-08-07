a08faf2192a8a888ae20fcadbcaad2f060fcdac8
Jonathan Wilchield Arthur(10945544) <100198275+jocypher@users.noreply.github.com>
2022-08-07 16:21:38 +0000

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,3 +1,10 @@
-for i in range(100):
-    if i % 2 == 0:
-       print(i)
+# A python code to find the average of the sum all even numbers between 1 and 10000
+sum = 1
+avg = 0
+for i in range(10000):
+   if i % 2 == 0:
+      sum += i
+      avg += 1
+      print(sum/avg)
+   
+  
