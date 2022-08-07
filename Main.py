331b7753337f29fcd4c37e835ee83c43207361c7
Jonathan Wilchield Arthur(10945544) <100198275+jocypher@users.noreply.github.com>
2022-08-07 16:04:18 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-# A python program to find the average of the sum the number of prime numbers from 1 to 10
+# A python program to find the average of the sum the number of prime numbers from 1 to 10 and 1 to 10000
 prime_numbers = [2,3,5,7]
 sum = prime_numbers[0] + prime_numbers[1] + prime_numbers[2]
 prime = sum + prime_numbers[3] 
@@ -9,3 +9,14 @@ print(prime)
 primes = len(prime_numbers )
 aver = prime/ primes
 print(aver)
+
+
+# OR
+
+sum = 1
+avg =0
+for i in range(10000):
+  if (i % 2 == 1 and i % 3 != 0) or (i == 3) :
+    sum += i
+    avg += 1
+    print(sum/ avg)
